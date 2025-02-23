e080495a840d11205daac01b791ce4c1a27c4819
EmreKose0 <emrekose1995@gmail.com>
2025-02-22 16:08:24 -0800

    proxy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import customFetch from "./utils/customFetch.js";
 
-createRoot(document.getElementById('root')).render(
+const resp = await customFetch.get("/test");
+console.log(resp);
+// fetch("/api/v1/test")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
